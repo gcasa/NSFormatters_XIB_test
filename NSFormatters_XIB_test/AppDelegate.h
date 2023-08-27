3fae01ba8bbe85d5165871bb0cfff4328f7f9619
Gregory Casamento <greg.casamento@gmail.com>
2023-08-27 14:34:44 -0400

    Initial Commit

---
+++ NSFormatters_XIB_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSFormatters_XIB_test
+//
+//  Created by Gregory John Casamento on 8/27/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
